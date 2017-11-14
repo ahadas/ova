7b8ecd9847aada47dcc9ec9dded24227727a0952
Arik Hadas <arik.hadas1@gmail.com>
2017-11-14 16:22:10 +0200

    adjust the export ova example to the modified api

--- export_ova.py
+++ export_ova.py
@@ -19,8 +19,9 @@ connection = sdk.Connection(
 
 vms_service = connection.system_service().vms_service()
 exported_vm = vms_service.list()[0]
-vms_service.vm_service(exported_vm.id).export(
-    host = types.Host(name='vm1'),
-    path='/tmp/arik.ova'
+vms_service.vm_service(exported_vm.id).export_to_path_on_host(
+    host = types.Host(id='ce5f4bd2-1c08-4deb-83a3-07574a56f323'),
+    directory='/tmp',
+    filename='arik987.ova'
 )
 
