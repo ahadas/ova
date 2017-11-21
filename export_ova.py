952628fb417381c3d9fd0e81dff4ffa480e8e462
ahadas@redhat.com <arik.hadas1@gmail.com>
2017-11-21 14:10:05 +0200

    pep8 - export ova

--- export_ova.py
+++ export_ova.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tarfile
-import re
+import logging
 import ovirtsdk4 as sdk
 import ovirtsdk4.types as types
-import logging
 import time
 
 connection = sdk.Connection(
@@ -20,8 +18,7 @@ connection = sdk.Connection(
 vms_service = connection.system_service().vms_service()
 exported_vm = vms_service.list()[0]
 vms_service.vm_service(exported_vm.id).export_to_path_on_host(
-    host = types.Host(id='ce5f4bd2-1c08-4deb-83a3-07574a56f323'),
+    host=types.Host(id='ce5f4bd2-1c08-4deb-83a3-07574a56f323'),
     directory='/tmp',
     filename='arik987.ova'
 )
-
