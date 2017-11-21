9f85bdaf5c37fb940e597472da0966e786e90df9
ahadas@redhat.com <arik.hadas1@gmail.com>
2017-11-21 14:40:35 +0200

    export ova

--- export_ova.py
+++ export_ova.py
@@ -7,18 +7,17 @@ import ovirtsdk4.types as types
 import time
 
 connection = sdk.Connection(
-    url='http://0.0.0.0:8080/ovirt-engine/api',
+    url='http://dubi.tlv.redhat.com:8080/ovirt-engine/api',
     username='admin@internal',
     password='1',
     debug=True,
     log=logging.getLogger(),
 )
 
-
 vms_service = connection.system_service().vms_service()
 exported_vm = vms_service.list()[0]
 vms_service.vm_service(exported_vm.id).export_to_path_on_host(
     host=types.Host(id='ce5f4bd2-1c08-4deb-83a3-07574a56f323'),
     directory='/tmp',
-    filename='arik987.ova'
+    filename='myvm.ova'
 )
